a087a1dd2688fbd5e8ecef102ab7e101929131b7
Haliz <foril2n1@rambler.ru>
2024-02-24 23:21:34 +0300

    Урок 5. Структура приложения. Создание базы данных (БД)

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("ROOT", dirname(__DIR__)); // dirname поднимает выше на один уровень.
+define("PUBLIC_DIR", __DIR__);
+define("CORE", ROOT . '/core');
+define("CONFIG", ROOT . '/config');
+define("APP", ROOT . '/app');
+define("CONTROLLERS", APP . '/controllers');
+define("VIEWS", APP . '/views');
+define("PATH", 'http://practicephp');
--- public/index.php
+++ public/index.php
@@ -1,21 +1,6 @@
 <?php
 
-define("ROOT", dirname(__DIR__)); // dirname поднимает выше на один уровень.
-define("PUBLIC_DIR", __DIR__);
-define("CORE", ROOT . '/core');
-define("APP", ROOT . '/app');
-define("CONTROLLERS", APP . '/controllers');
-define("VIEWS", APP . '/views');
-define("PATH", 'http://practicephp');
+require_once dirname(__DIR__) . '/config/config.php';
 
 require_once CORE . '/funcs.php';
-
-$uri = trim(parse_url( $_SERVER['REQUEST_URI'])['path'], '/');
-if ($uri === '') {
-    require CONTROLLERS . '/index.php';
-} elseif ($uri == 'about') {
-    require CONTROLLERS . '/about.php';
-} else {
-    abort();
-}
-
+require_once CORE . '/router.php';
