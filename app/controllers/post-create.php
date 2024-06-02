aee5ad8d41ffa3841a4aeb50dff39e1ad74672e1
Haliz <foril2n1@rambler.ru>
2024-06-02 20:57:20 +0300

    Урок 11. Validator - класс для валидации данных

--- app/controllers/post-create.php
+++ app/controllers/post-create.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once CORE . '/classes/Validator.php';
 /**
  * @var Db $db
  */
@@ -7,22 +7,46 @@
 //dump($_SERVER['REQUEST_METHOD']);
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $fillable = ['title', 'content', 'excerpt'];
     $data = load($fillable);
 
 //    Validation
-    $errors = [];
-    if (empty($data['title'])) {
-        $errors['title'] = 'Title is required';
-    }
-    if (empty($data['content'])) {
-        $errors['content'] = 'Content is required';
-    }
-    if (empty($data['excerpt'])) {
-        $errors['excerpt'] = 'Excerpt is required';
-    }
-    if (empty($errors) ) {
+    $validator = new Validator();
+    $validation = $validator->validate($data, [
+        'title' => [
+            'required' => true,
+            'min' => 5,
+            'max' => 190,
+        ],
+        'excerpt' => [
+            'required' => true,
+            'min' => 10,
+            'max' => 190,
+        ],
+        'content' => [
+            'required' => true,
+            'min' => 100,
+        ],
+    ]);
+        if($validation->hasErrors()){
+            print_arr($validation->getErrors());
+        } else {
+            echo 'SUCCESS';
+        }
+
+    die;
+
+    /* if (empty($data['title'])) {
+         $errors['title'] = 'Title is required';
+     }
+     if (empty($data['content'])) {
+         $errors['content'] = 'Content is required';
+     }
+     if (empty($data['excerpt'])) {
+         $errors['excerpt'] = 'Excerpt is required';
+     }*/
+    if (empty($errors)) {
         if ($db->query("INSERT INTO posts (title, content, excerpt) VALUES (:title, :content, :excerpt)", $data)) {
             echo 'OK';
         } else {
@@ -35,4 +59,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 }
 $title = "My Blog :: New post";
 
-require_once VIEWS.'/post-create.tpl.php';
+require_once VIEWS . '/post-create.tpl.php';
